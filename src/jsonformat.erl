26a406646117510d84f5777db9b27b146416cee8
Dmitry Sviderskiy <svdmitrij@gmail.com>
2022-04-21 16:51:23 +0400

    Add support key mappings and format funs for meta

--- src/jsonformat.erl
+++ src/jsonformat.erl
@@ -27,10 +27,10 @@
 -export([system_time_to_iso8601_nano/1]).
 
 %%%_* Types ============================================================
--type config() :: #{ new_line => boolean()
-                   , key_mapping => #{atom() => atom()}
-                   , format_funs => #{atom() => fun((_) -> _)}
-                   , chars_limit => non_neg_integer()}.
+-type config() :: #{new_line => boolean()
+, key_mapping => #{atom() => atom()}
+, format_funs => #{atom() => fun((_) -> _)}
+, chars_limit => non_neg_integer()}.
 
 -export_type([config/0]).
 
@@ -41,101 +41,103 @@
 %%%_* Code =============================================================
 %%%_ * API -------------------------------------------------------------
 -spec format(logger:log_event(), config()) -> unicode:chardata().
-format(#{msg:={report, #{format:=Format, args:=Args, label:={error_logger, _}}}} = Map, Config) ->
-  Report = #{text => io_lib:format(Format, Args, chars_limit(Config))},
-  format(Map#{msg := {report, Report}}, Config);
-format(#{level:=Level, msg:={report, Msg}, meta:=Meta}, Config) when is_map(Msg) ->
-  Data0 = Msg,
-  Data1 = apply_key_mapping(Data0, Config),
-  Data2 = apply_format_funs(Data1, Config),
-  encode(#{report => [pre_encode(Meta#{level => Level}, Config), pre_encode(Data2, Config)]}, Config);
+format(#{msg := {report, #{format := Format, args := Args, label := {error_logger, _}}}} = Map, Config) ->
+    Report = #{text => io_lib:format(Format, Args, chars_limit(Config))},
+    format(Map#{msg := {report, Report}}, Config);
+format(#{level := Level, msg := {report, Msg}, meta := Meta}, Config) when is_map(Msg) ->
+    Data0 = Msg,
+    Data1 = apply_key_mapping(Data0, Config),
+    Data2 = apply_format_funs(Data1, Config),
+    Meta1 = apply_key_mapping(Meta#{level => Level}, Config),
+    Meta2 = apply_format_funs(Meta1, Config),
+    encode(#{report => [pre_encode(Meta2, Config), pre_encode(Data2, Config)]}, Config);
 format(Map = #{msg := {report, KeyVal}}, Config) when is_list(KeyVal) ->
-  format(Map#{msg := {report, maps:from_list(KeyVal)}}, Config);
+    format(Map#{msg := {report, maps:from_list(KeyVal)}}, Config);
 format(Map = #{msg := {string, String}}, Config) ->
-  Report = #{text => unicode:characters_to_binary(String)},
-  format(Map#{msg := {report, Report}}, Config);
+    Report = #{text => unicode:characters_to_binary(String)},
+    format(Map#{msg := {report, Report}}, Config);
 format(Map = #{msg := {Format, Terms}}, Config) ->
-  format(Map#{msg := {string, io_lib:format(Format, Terms, chars_limit(Config))}}, Config).
-
-chars_limit(Config) ->
-    [{chars_limit, maps:get(chars_limit, Config, -1)}].
+    format(Map#{msg := {string, io_lib:format(Format, Terms, chars_limit(Config))}}, Config).
 
 %%% Useful for converting logger:timestamp() to a readable timestamp.
 -spec system_time_to_iso8601(integer()) -> binary().
 system_time_to_iso8601(Epoch) ->
-  system_time_to_iso8601(Epoch, microsecond).
+    system_time_to_iso8601(Epoch, microsecond).
 
 -spec system_time_to_iso8601_nano(integer()) -> binary().
 system_time_to_iso8601_nano(Epoch) ->
-  system_time_to_iso8601(1000 * Epoch, nanosecond).
+    system_time_to_iso8601(1000 * Epoch, nanosecond).
 
 -spec system_time_to_iso8601(integer(), erlang:time_unit()) -> binary().
 system_time_to_iso8601(Epoch, Unit) ->
-  binary:list_to_bin(calendar:system_time_to_rfc3339(Epoch, [{unit, Unit}, {offset, "Z"}])).
+    binary:list_to_bin(calendar:system_time_to_rfc3339(Epoch, [{unit, Unit}, {offset, "Z"}])).
 
 %%%_* Private functions ================================================
+chars_limit(Config) ->
+    [{chars_limit, maps:get(chars_limit, Config, -1)}].
+
 pre_encode(Data, Config) ->
-  maps:fold(
-    fun(K, V, Acc) when is_map(V) ->
-      maps:put(jsonify(K, Config), pre_encode(V, Config), Acc);
-       (K, Vs, Acc) when is_list(Vs), is_map(hd(Vs)) -> % assume list of maps
-      maps:put(jsonify(K, Config), [pre_encode(V, Config) || V <- Vs, is_map(V)], Acc);
-       (K, V, Acc) ->
-      maps:put(jsonify(K, Config), jsonify(V, Config), Acc)
-    end,
-    maps:new(),
-    Data).
+    maps:fold(
+        fun(K, V, Acc) when is_map(V) ->
+            maps:put(jsonify(K, Config), pre_encode(V, Config), Acc);
+            (K, Vs, Acc) when is_list(Vs), is_map(hd(Vs)) -> % assume list of maps
+                maps:put(jsonify(K, Config), [pre_encode(V, Config) || V <- Vs, is_map(V)], Acc);
+            (K, V, Acc) ->
+                maps:put(jsonify(K, Config), jsonify(V, Config), Acc)
+        end,
+        maps:new(),
+        Data).
 
 encode(Data, Config) ->
-  Json = jsx:encode(Data),
-  case new_line(Config) of
-    true -> [Json, <<"\n">>];
-    false -> Json
-  end.
+    Json = jsx:encode(Data),
+    case new_line(Config) of
+        true -> [Json, <<"\n">>];
+        false -> Json
+    end.
 
-jsonify(A, _Config) when is_atom(A)     -> A;
-jsonify(B, _Config) when is_binary(B)   -> B;
-jsonify(I, _Config) when is_integer(I)  -> I;
-jsonify(F, _Config) when is_float(F)    -> F;
-jsonify(B, _Config) when is_boolean(B)  -> B;
-jsonify(P, Config) when is_pid(P)      -> jsonify(pid_to_list(P), Config);
-jsonify(P, Config) when is_port(P)     -> jsonify(port_to_list(P), Config);
+jsonify(A, _Config) when is_atom(A) -> A;
+jsonify(B, _Config) when is_binary(B) -> B;
+jsonify(I, _Config) when is_integer(I) -> I;
+jsonify(F, _Config) when is_float(F) -> F;
+jsonify(B, _Config) when is_boolean(B) -> B;
+jsonify(P, Config) when is_pid(P) -> jsonify(pid_to_list(P), Config);
+jsonify(P, Config) when is_port(P) -> jsonify(port_to_list(P), Config);
 jsonify(F, Config) when is_function(F) -> jsonify(erlang:fun_to_list(F), Config);
-jsonify(L, Config) when is_list(L)     ->
-  try list_to_binary(L) of
-    S -> S
-  catch error:badarg ->
-    unicode:characters_to_binary(io_lib:format("~0p", [L], chars_limit(Config)))
-  end;
+jsonify(L, Config) when is_list(L) ->
+    try list_to_binary(L) of
+        S -> S
+    catch error:badarg ->
+        unicode:characters_to_binary(io_lib:format("~0p", [L], chars_limit(Config)))
+    end;
 jsonify({M, F, A}, _Config) when is_atom(M), is_atom(F), is_integer(A) ->
-  <<(a2b(M))/binary,$:,(a2b(F))/binary,$/,(integer_to_binary(A))/binary>>;
-jsonify(Any, Config)                   -> unicode:characters_to_binary(io_lib:format("~0p", [Any], chars_limit(Config))).
+    <<(a2b(M))/binary, $:, (a2b(F))/binary, $/, (integer_to_binary(A))/binary>>;
+jsonify(Any, Config) -> unicode:characters_to_binary(io_lib:format("~0p", [Any], chars_limit(Config))).
 
 a2b(A) -> atom_to_binary(A, utf8).
 
 apply_format_funs(Data, #{format_funs := Callbacks}) ->
-  maps:fold(fun(K, Fun, Acc) when is_map_key(K, Data) -> maps:update_with(K, Fun, Acc);
-               (_, _, Acc)                            -> Acc
-            end, Data, Callbacks);
+    maps:fold(fun(K, Fun, Acc) when is_map_key(K, Data) -> maps:update_with(K, Fun, Acc);
+        (_, _, Acc) -> Acc
+              end, Data, Callbacks);
 apply_format_funs(Data, _) ->
-  Data.
+    Data.
 
 apply_key_mapping(Data, #{key_mapping := Mapping}) ->
-  DataOnlyMapped =
-    maps:fold(fun(K, V, Acc) when is_map_key(K, Data) -> Acc#{V => maps:get(K, Data)};
-                 (_, _, Acc)                          -> Acc
-              end, #{}, Mapping),
-  DataNoMapped = maps:without(maps:keys(Mapping), Data),
-  maps:merge(DataNoMapped, DataOnlyMapped);
+    DataOnlyMapped =
+        maps:fold(fun(K, V, Acc) when is_map_key(K, Data) -> Acc#{V => maps:get(K, Data)};
+            (_, _, Acc) -> Acc
+                  end, #{}, Mapping),
+    DataNoMapped = maps:without(maps:keys(Mapping), Data),
+    maps:merge(DataNoMapped, DataOnlyMapped);
 apply_key_mapping(Data, _) ->
-  Data.
+    Data.
 
 new_line(Config) -> maps:get(new_line, Config, ?NEW_LINE).
 
 meta_without(Meta, Config) ->
     maps:without(maps:get(meta_without, Config, [report_cb]), Meta).
 
-meta_with(Meta, #{ meta_with := Ks}) ->
+meta_with(Meta, #{meta_with := Ks}) ->
     maps:with(Ks, Meta);
 meta_with(Meta, _ConfigNotPresent) ->
     Meta.
@@ -145,81 +147,87 @@ meta_with(Meta, _ConfigNotPresent) ->
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
+%%noinspection ErlangUnresolvedMacros
 format_test() ->
-  ?assertEqual( <<"{\"level\":\"alert\",\"text\":\"derp\"}">>
-              , format(#{level => alert, msg => {string, "derp"}, meta => #{}}, #{}) ),
-  ?assertEqual( <<"{\"herp\":\"derp\",\"level\":\"alert\"}">>
-              , format(#{level => alert, msg => {report, #{herp => derp}}, meta => #{}}, #{}) ).
+    ?assertEqual(<<"{\"level\":\"alert\",\"text\":\"derp\"}">>
+        , format(#{level => alert, msg => {string, "derp"}, meta => #{}}, #{})),
+    ?assertEqual(<<"{\"herp\":\"derp\",\"level\":\"alert\"}">>
+        , format(#{level => alert, msg => {report, #{herp => derp}}, meta => #{}}, #{})).
 
+%%noinspection ErlangUnresolvedMacros
 format_funs_test() ->
-  Config1 = #{format_funs => #{ time  => fun(Epoch) -> Epoch + 1 end
-                              , level => fun(alert) -> info      end}},
-  ?assertEqual( <<"{\"level\":\"info\",\"text\":\"derp\",\"time\":2}">>
-              , format(#{level => alert, msg => {string, "derp"}, meta => #{time => 1}}, Config1)),
+    Config1 = #{format_funs => #{time => fun(Epoch) -> Epoch + 1 end
+        , level => fun(alert) -> info end}},
+    ?assertEqual(<<"{\"level\":\"info\",\"text\":\"derp\",\"time\":2}">>
+        , format(#{level => alert, msg => {string, "derp"}, meta => #{time => 1}}, Config1)),
 
-  Config2 = #{format_funs => #{ time  => fun(Epoch) -> Epoch + 1 end
-                              , foobar => fun(alert) -> info      end}},
-  ?assertEqual( <<"{\"level\":\"alert\",\"text\":\"derp\",\"time\":2}">>
-              , format(#{level => alert, msg => {string, "derp"}, meta => #{time => 1}}, Config2)).
+    Config2 = #{format_funs => #{time => fun(Epoch) -> Epoch + 1 end
+        , foobar => fun(alert) -> info end}},
+    ?assertEqual(<<"{\"level\":\"alert\",\"text\":\"derp\",\"time\":2}">>
+        , format(#{level => alert, msg => {string, "derp"}, meta => #{time => 1}}, Config2)).
 
+%%noinspection ErlangUnresolvedMacros
 key_mapping_test() ->
-  Config1 = #{key_mapping => #{ level => lvl
-                              , text => message}},
-  ?assertEqual( <<"{\"lvl\":\"alert\",\"message\":\"derp\"}">>
-              , format(#{level => alert, msg => {string, "derp"}, meta => #{}}, Config1)),
+    Config1 = #{key_mapping => #{level => lvl
+        , text => message}},
+    ?assertEqual(<<"{\"lvl\":\"alert\",\"message\":\"derp\"}">>
+        , format(#{level => alert, msg => {string, "derp"}, meta => #{}}, Config1)),
 
-  Config2 = #{key_mapping => #{ level => lvl
-                              , text => level}},
-  ?assertEqual( <<"{\"level\":\"derp\",\"lvl\":\"alert\"}">>
-              , format(#{level => alert, msg => {string, "derp"}, meta => #{}}, Config2)),
+    Config2 = #{key_mapping => #{level => lvl
+        , text => level}},
+    ?assertEqual(<<"{\"level\":\"derp\",\"lvl\":\"alert\"}">>
+        , format(#{level => alert, msg => {string, "derp"}, meta => #{}}, Config2)),
 
-  Config3 = #{key_mapping => #{ level => lvl
-                              , foobar => level}},
-  ?assertEqual( <<"{\"lvl\":\"alert\",\"text\":\"derp\"}">>
-              , format(#{level => alert, msg => {string, "derp"}, meta => #{}}, Config3)),
+    Config3 = #{key_mapping => #{level => lvl
+        , foobar => level}},
+    ?assertEqual(<<"{\"lvl\":\"alert\",\"text\":\"derp\"}">>
+        , format(#{level => alert, msg => {string, "derp"}, meta => #{}}, Config3)),
 
-  Config4 = #{ key_mapping => #{time => timestamp}
-             , format_funs => #{timestamp => fun(T) -> T + 1 end}},
-  ?assertEqual( <<"{\"level\":\"alert\",\"text\":\"derp\",\"timestamp\":2}">>
-              , format(#{level => alert, msg => {string, "derp"}, meta => #{time => 1}}, Config4)).
+    Config4 = #{key_mapping => #{time => timestamp}
+        , format_funs => #{timestamp => fun(T) -> T + 1 end}},
+    ?assertEqual(<<"{\"level\":\"alert\",\"text\":\"derp\",\"timestamp\":2}">>
+        , format(#{level => alert, msg => {string, "derp"}, meta => #{time => 1}}, Config4)).
 
+%%noinspection ErlangUnresolvedMacros
 list_format_test() ->
     ErrorReport =
         #{level => error,
-          meta => #{time => 1},
-          msg => {report,#{report => [{hej,"hopp"}]}}},
-    ?assertEqual( <<"{\"level\":\"error\",\"report\":\"[{hej,\\\"hopp\\\"}]\",\"time\":1}">>
-                , format(ErrorReport, #{})).
+            meta => #{time => 1},
+            msg => {report, #{report => [{hej, "hopp"}]}}},
+    ?assertEqual(<<"{\"level\":\"error\",\"report\":\"[{hej,\\\"hopp\\\"}]\",\"time\":1}">>
+        , format(ErrorReport, #{})).
 
+%%noinspection ErlangUnresolvedMacros
 meta_without_test() ->
-    Error = #{ level => info
-             , msg => {report, #{answer => 42}}
-             , meta => #{secret => xyz}},
-    ?assertEqual([ {<<"answer">>, 42}
-                 , {<<"level">>, <<"info">>}
-                 , {<<"secret">>, <<"xyz">>}
-                 ],
+    Error = #{level => info
+        , msg => {report, #{answer => 42}}
+        , meta => #{secret => xyz}},
+    ?assertEqual([{<<"answer">>, 42}
+        , {<<"level">>, <<"info">>}
+        , {<<"secret">>, <<"xyz">>}
+    ],
         jsx:decode(format(Error, #{}))),
-    Config2 = #{ meta_without => [secret]},
-    ?assertEqual([ {<<"answer">>, 42}
-                 , {<<"level">>, <<"info">>}
-                 ],
+    Config2 = #{meta_without => [secret]},
+    ?assertEqual([{<<"answer">>, 42}
+        , {<<"level">>, <<"info">>}
+    ],
         jsx:decode(format(Error, Config2))),
     ok.
 
+%%noinspection ErlangUnresolvedMacros
 meta_with_test() ->
-    Error = #{ level => info
-             , msg => {report, #{answer => 42}}
-             , meta => #{secret => xyz}},
-    ?assertEqual([ {<<"answer">>, 42}
-                 , {<<"level">>, <<"info">>}
-                 , {<<"secret">>, <<"xyz">>}
-                 ],
+    Error = #{level => info
+        , msg => {report, #{answer => 42}}
+        , meta => #{secret => xyz}},
+    ?assertEqual([{<<"answer">>, 42}
+        , {<<"level">>, <<"info">>}
+        , {<<"secret">>, <<"xyz">>}
+    ],
         jsx:decode(format(Error, #{}))),
-    Config2 = #{ meta_with => [level]},
-    ?assertEqual([ {<<"answer">>, 42}
-                 , {<<"level">>, <<"info">>}
-                 ],
+    Config2 = #{meta_with => [level]},
+    ?assertEqual([{<<"answer">>, 42}
+        , {<<"level">>, <<"info">>}
+    ],
         jsx:decode(format(Error, Config2))),
     ok.
 
